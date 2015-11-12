8eacf4e1b2a7ed27341868794db209d8eef95bbc
pallavJha <pallav_kumar71@yahoo.com>
2015-11-13 01:04:14 +0530

    Session in Base Controller

--- webapp/src/main/java/pl/imguploadimg/webapp/controller/BaseController.java
+++ webapp/src/main/java/pl/imguploadimg/webapp/controller/BaseController.java
@@ -1,5 +1,8 @@
 package pl.imguploadimg.webapp.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BaseController {
 
 	@RequestMapping("home")
-	public String functionWithoutName(){
+	public String functionWithoutName(HttpServletRequest req, HttpSession session){
 		return "home";
 	}
 }
