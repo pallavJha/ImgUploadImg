5d3d42005a761755e6a8ed052d33dc4c226d8677
pallavJha <pallav_kumar71@yahoo.com>
2016-02-09 16:21:58 +0530

    asad

--- webapp/src/main/java/pl/imguploadimg/webapp/threads/LinkConsumer.java
+++ webapp/src/main/java/pl/imguploadimg/webapp/threads/LinkConsumer.java
@@ -2,7 +2,10 @@ package pl.imguploadimg.webapp.threads;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+
+import org.apache.commons.validator.routines.UrlValidator;
 
 import pl.imguploadimg.base.log.LoggerService;
 
@@ -13,6 +16,7 @@ public class LinkConsumer extends Thread {
 	private String protocol;
 	private String protocolHost;
 	private LoggerService loggerService;
+	static UrlValidator urlValidator = new UrlValidator();
 
 	public LinkConsumer(List<String> anchorList, URL url, String protocol,String protocolHost, LoggerService loggerService) {
 		
@@ -24,8 +28,9 @@ public class LinkConsumer extends Thread {
 		this.loggerService = loggerService;
 	}
 	
-	public void findImagesFromHTML(String htmlString) {
-		ArrayList<String> imgList = new ArrayList<String>();
+	public void findImagesFromHTML(String htmlString, String protocol, String protocolHost) {
+		LinkedList<String> imgList = new LinkedList<String>();
+		LinkedList<String> imgUrlList = new LinkedList<String>();
 		String str = htmlString;
 		char[] cbuf = str.toCharArray();
 		for (int i = 0; i < cbuf.length; i++) {
@@ -44,7 +49,53 @@ public class LinkConsumer extends Thread {
 		}
 		loggerService.log(imgList.size() + " image(s) found.");
 		for (String s : imgList) {
-			loggerService.log(s);
+			complete: for (int i = 0; i < s.length(); i++){
+				if(s.charAt(i)=='s' && s.charAt(i+1) == 'r' && s.charAt(i+2) == 'c'){
+					for (int j = i+3; j < s.length(); j++) {
+						if(s.charAt(j) == '='){
+							for (int k = j; k < s.length(); k++) {
+								if(s.charAt(k) == '\"' || s.charAt(k) == '\''){
+									for (int l = k+1; l < s.length(); l++) {
+										if(s.charAt(l) == '\"' || s.charAt(l) == '\''){
+											imgUrlList.add(s.substring(k+1, l).trim());
+											break complete;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		imgList.clear();
+		for(int i = 0; i< imgUrlList.size();i++){
+			String s = imgUrlList.get(i);
+			if(s.length() > 2  && s.charAt(0) == '/' && s.charAt(1) == '/'){
+				s = protocol +":"+ s;
+				imgUrlList.remove(i);
+				imgUrlList.add(i, s);
+			}
+			else if(s.length() > 2  && s.charAt(0) == '/'){
+				s = protocolHost + s;
+				imgUrlList.remove(i);
+				imgUrlList.add(i, s);
+			}
+			if(!urlValidator.isValid(s)){
+				imgUrlList.remove(i);
+			}
+			else {
+				imgList.add(imgUrlList.get(i));
+			}
+		}
+		imgUrlList.clear();
+		
+		for(String s : imgList){
+			StringBuilder imgElement = new StringBuilder("<img src=\"");
+			imgElement.append(s);
+			imgElement.append("\" width=\"200\" height=\"200\">");
+			imgUrlList.add(imgElement.toString());
+			loggerService.log(imgElement.toString());
 		}
 	}
 	
